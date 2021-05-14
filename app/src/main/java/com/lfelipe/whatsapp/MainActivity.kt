c38c27c8924c5e3643fc501af872fe309c5cab68
Luis Felipe Maior <lfelipe.maior@gmail.com>
2021-05-13 23:14:00 -0300

    Layout tela principal

---
+++ app/src/main/java/com/lfelipe/whatsapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.lfelipe.whatsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lfelipe.whatsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+    }
+}
